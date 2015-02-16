1227ebb8410491bec0335645869ff8b826ce5c19
bpesquet <bpesquet@gmail.com>
2015-02-16 13:41:10 +0100

    Deployment on Heroku

--- app/config/dev.php
+++ app/config/dev.php
@@ -1,7 +1,15 @@
 <?php
 
-// include the prod configuration
-require __DIR__.'/prod.php';
+// Doctrine (db)
+$app['db.options'] = array(
+    'driver'   => 'pdo_mysql',
+    'charset'  => 'utf8',
+    'host'     => 'localhost',
+    'port'     => '3306',
+    'dbname'   => 'gsb',
+    'user'     => 'gsb_user',
+    'password' => 'secret',
+);
 
 // enable the debug mode
 $app['debug'] = true;
